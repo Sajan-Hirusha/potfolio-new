f82b2fa55f00e9b7cff64f0a54470618dbce1ef9
Sajan-Hirusha <141469578+Sajan-Hirusha@users.noreply.github.com>
2025-04-11 14:53:13 +0530

    add config files

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,7 +4,11 @@ import BaseHome from "./screens/BaseHome/BaseHome.jsx";
 function App() {
 
   return (
+<<<<<<< Updated upstream
       <BrowserRouter>
+=======
+      <BrowserRouter  basename="/potfolio-new">
+>>>>>>> Stashed changes
           <Routes>
               <Route element={<BaseHome/>} path='/'/>
           </Routes>
